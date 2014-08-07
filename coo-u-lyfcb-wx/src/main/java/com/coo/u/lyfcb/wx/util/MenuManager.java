bc6624ff8a8151406f6b58d6e1418fe795fc45a5
sunbingjue <sunbingjue.aiai@163.com>
2014-08-07 15:09:03 +0800

    menu

--- coo-u-lyfcb-wx/src/main/java/com/coo/u/lyfcb/wx/util/MenuManager.java
+++ coo-u-lyfcb-wx/src/main/java/com/coo/u/lyfcb/wx/util/MenuManager.java
@@ -1,6 +1,8 @@
 package com.coo.u.lyfcb.wx.util;
 
 import com.coo.u.lyfcb.wx.INameSpace;
+import com.kingstar.ngbf.s.weixin.model.Button;
+import com.kingstar.ngbf.s.weixin.model.ComplexButton;
 import com.kingstar.ngbf.s.weixin.model.Menu;
 import com.kingstar.ngbf.s.weixin.model.WeixinToken;
 import com.kingstar.ngbf.s.weixin.util.WeixinUtil;
@@ -12,8 +14,8 @@ import com.kingstar.ngbf.s.weixin.util.WeixinUtil;
  * @date 2013-07-28
  */
 public class MenuManager {
-	
-	public static void main(String[] args) {
+
+	public static void main11(String[] args) {
 		// 调用接口获取access_token
 		WeixinToken at = WeixinUtil.getAccessToken(INameSpace.APP_ID,
 				INameSpace.APP_SECRET);
@@ -21,13 +23,18 @@ public class MenuManager {
 		if (null != at) {
 			// 调用接口创建菜单
 			int result = WeixinUtil.createMenu(getMenu(), at.getToken());
-			// 调用接口删除菜单
-			// int result = WeixinUtil.deleteMenu(at.getToken());
 			// 判断菜单创建结果
 			if (0 == result)
 				System.out.println("菜单创建成功！");
 			else
 				System.out.println("菜单创建失败，错误码：" + result);
+			// 调用接口删除菜单
+			// int result = WeixinUtil.deleteMenu(at.getToken());
+			// 判断菜单删除结果
+            // if (0 == result)
+            // System.out.println("菜单删除成功！");
+            // else
+            // System.out.println("菜单删除失败，错误码：" + result);
 		}
 	}
 
@@ -37,83 +44,43 @@ public class MenuManager {
 	 * @return
 	 */
 	private static Menu getMenu() {
-		// CommonViewButton btn11 = new CommonViewButton();
-		// btn11.setName("双色球");
-		// btn11.setType("view");
-		// btn11.setUrl("http://www.lyflcp.com.cn/");
-		//
-		// CommonViewButton btn12 = new CommonViewButton();
-		// btn12.setName("福彩3D");
-		// btn12.setType("view");
-		// btn12.setUrl("http://www.lyflcp.com.cn/");
-		// CommonViewButton btn13 = new CommonViewButton();
-		// btn13.setName("东方6+1");
-		// btn13.setType("view");
-		// btn13.setUrl("http://www.lyflcp.com.cn/");
-		//
-		// CommonViewButton btn14 = new CommonViewButton();
-		// btn14.setName("河南幸运彩");
-		// btn14.setType("view");
-		// btn14.setUrl("http://www.lyflcp.com.cn/");
-		//
-		// CommonViewButton btn15 = new CommonViewButton();
-		// btn15.setName("购彩大厅");
-		// btn15.setType("view");
-		// btn15.setUrl("http://www.lyflcp.com.cn/");
-		//
-		// CommonViewButton btn21 = new CommonViewButton();
-		// btn21.setName("站点地图");
-		// btn21.setType("view");
-		// btn21.setUrl("http://www.lyflcp.com.cn/");
-		//
-		// CommonClickButton btn22 = new CommonClickButton();
-		// btn22.setName("在线申请");
-		// btn22.setType("click");
-		// btn22.setKey(INameSpace.MENU_KEY_ZXSQ);
-		//
-		// CommonClickButton btn23 = new CommonClickButton();
-		// btn23.setName("办卡须知");
-		// btn23.setType("click");
-		// btn23.setKey(INameSpace.MENU_KEY_BKXZ);
-		//
-		// CommonClickButton btn24 = new CommonClickButton();
-		// btn24.setName("在线客服");
-		// btn24.setType("click");
-		// btn24.setKey(INameSpace.MENU_KEY_ZXKF);
-		//
-		// CommonViewButton btn31 = new CommonViewButton();
-		// btn31.setName("乐在骑中");
-		// btn31.setType("view");
-		// btn31.setUrl("http://www.lyflcp.com.cn/");
-		//
-		// CommonViewButton btn32 = new CommonViewButton();
-		// btn32.setName("下载客户端");
-		// btn32.setType("view");
-		// btn32.setUrl("http://www.lyflcp.com.cn/");
-		//
-		// // ///////////////////////////////////////////
-		// ComplexButton mainBtn1 = new ComplexButton();
-		// mainBtn1.setName("买彩票");
-		// mainBtn1.setSub_button(new Button[] { btn11, btn12, btn13, btn14 });
-		//
-		// ComplexButton mainBtn2 = new ComplexButton();
-		// mainBtn2.setName("公益自行车");
-		// mainBtn2.setSub_button(new Button[] { btn21, btn22, btn23, btn24 });
-		//
-		// ComplexButton mainBtn3 = new ComplexButton();
-		// mainBtn3.setName("公益活动");
-		// mainBtn3.setSub_button(new Button[] { btn31, btn32 });
-		// /**
-		// * 这是公众号目前的菜单结构，每个一级菜单都有二级菜单项<br>
-		// *
-		// * 在某个一级菜单下没有二级菜单的情况，menu该如何定义呢？<br>
-		// * 比如，第三个一级菜单项不是“更多体验”，而直接是“幽默笑话”，那么menu应该这样定义：<br>
-		// * menu.setButton(new Button[] { mainBtn1, mainBtn2, btn33 });
-		// */
-		// Menu menu = new Menu();
-		// menu.setButton(new Button[] { mainBtn1, mainBtn2, mainBtn3 });
+		Button btn11 = Button.view().name("双色球")
+				.url("http://www.lyflcp.com.cn/");
+		Button btn12 = Button.view().name("福彩3D")
+				.url("http://www.lyflcp.com.cn/");
+		Button btn13 = Button.view().name("东方6+1")
+				.url("http://www.lyflcp.com.cn/");
+		Button btn14 = Button.view().name("河南幸运彩")
+				.url("http://www.lyflcp.com.cn/");
+		Button btn15 = Button.view().name("购彩大厅")
+				.url("http://www.lyflcp.com.cn/");
 
-		// return menu;
-		return null;
+		////////////////////////////////////////////////
+		Button btn21 = Button.view().name("站点地图")
+				.url("http://www.lyflcp.com.cn/");
+		Button btn22 = Button.click().name("在线申请")
+				.key(INameSpace.MENU_KEY_ZXSQ);
+		Button btn23 = Button.click().name("办卡须知")
+				.key(INameSpace.MENU_KEY_BKXZ);
+		Button btn24 = Button.click().name("在线客服")
+				.key(INameSpace.MENU_KEY_ZXKF);
+
+		// //////////////////////////////////////////////
+		Button btn31 = Button.view().name("乐在骑中")
+				.url("http://www.lyflcp.com.cn/");
+		Button btn32 = Button.view().name("下载客户端")
+				.url("http://www.lyflcp.com.cn/");
+
+		ComplexButton mainBtn1 = new ComplexButton("买彩票").buttons(btn11, btn12,
+				btn13, btn14, btn15);
+		ComplexButton mainBtn2 = new ComplexButton("公益自行车").buttons(btn21,
+				btn22, btn23, btn24);
+		ComplexButton mainBtn3 = new ComplexButton("公益活动")
+				.buttons(btn31, btn32);
+
+		Menu menu = new Menu(mainBtn1, mainBtn2, mainBtn3);
+//		String json = menu.toJson();
+//		System.out.println(json);
+		return menu;
 	}
 }
